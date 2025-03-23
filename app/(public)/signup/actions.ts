e29789113dcf67b9b35af4b2a9ac33356c92d332
PradalCyril <pradal.cyril@gmail.com>
2025-03-23 17:09:43 +0100

    update supabase-auth template

--- app/(public)/signup/actions.ts
+++ app/(public)/signup/actions.ts
@@ -1,27 +1,42 @@
-'use server';
+"use server";
 
-import { createSSRClient } from '@/services/supabase';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+import { createSSRClient } from "@/services/supabase";
+import { AuthError } from "@supabase/supabase-js";
+// import { revalidatePath } from "next/cache";
+// import { redirect } from "next/navigation";
 
-export async function signup(formData: FormData) {
+type State = {
+  message: string;
+  error: AuthError | null;
+  isSuccess: boolean;
+};
+
+export async function signup(prevState: State, formData: FormData) {
   const supabase = await createSSRClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
   };
 
-  if (!data.email || !data.password) return;
+  if (!data.email || !data.password) {
+    return {
+      message: "Email and password are required",
+      error: new AuthError("Email and password are required"),
+      isSuccess: false,
+    };
+  }
 
   const { error } = await supabase.auth.signUp(data);
 
-  if (error) {
-    redirect('/error');
-  }
+  return error
+    ? { message: "", error, isSuccess: false }
+    : {
+        message: "Account created successfully, please verify your email",
+        error: null,
+        isSuccess: true,
+      };
 
-  revalidatePath('/', 'layout');
-  redirect('/');
+  // revalidatePath("/", "layout");
+  // redirect("/login", );
 }
--- app/(public)/signup/page.tsx
+++ app/(public)/signup/page.tsx
@@ -1,25 +1,59 @@
-import { AuthForm } from '@/components/auth-form';
-import { GalleryVerticalEnd } from 'lucide-react';
-import Link from 'next/link';
+"use client";
 
-import { signup } from './actions';
+import { AuthForm } from "@/components/auth-form";
+import { GalleryVerticalEnd } from "lucide-react";
+import Link from "next/link";
+
+import { signup } from "./actions";
+import { AuthError } from "@supabase/supabase-js";
+import { toast } from "sonner";
+import { useActionState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const initialState: {
+  message: string;
+  error: AuthError | null;
+  isSuccess: boolean;
+} = { message: "", error: null, isSuccess: false };
 
 export default function SignupPage() {
+  // TODO: Change this to your app name
+  const yourAppName = "Your App Name";
+
+  const router = useRouter();
+
+  const [state, formAction] = useActionState(signup, initialState);
+  console.log("🚀 ~ SignupPage ~ state:", state);
+
+  useEffect(() => {
+    if(!state.isSuccess && !state.error) {
+      return
+    }
+    if (!state.isSuccess && state.error) {
+      toast.error(state.error?.message);
+      return;
+    }
+    toast.success(state.message);
+    router.push("/login");
+  }, [state.isSuccess, state.message, router, state.error]);
   return (
     <>
-      <div className='bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10'>
-        <div className='flex w-full max-w-sm flex-col gap-6'>
-          <Link href='/' className='flex items-center gap-2 self-center font-medium'>
-            <div className='bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md'>
-              <GalleryVerticalEnd className='size-4' />
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <Link
+            href="/"
+            className="flex items-center gap-2 self-center font-medium"
+          >
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
             </div>
-            Fluxo
+            {yourAppName}
           </Link>
           <AuthForm
-            type='signup'
-            onEmailAuth={signup}
-            title='Welcome to ...'
-            description='Create your account with your Email'
+            type="signup"
+            onEmailAuth={formAction}
+            title="Welcome to ..."
+            description="Create your account with your Email"
           />
         </div>
       </div>
