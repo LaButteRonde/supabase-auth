085af12dc55f30ac5d6711ef84d7052d67817f16
PradalCyril <pradal.cyril@gmail.com>
2025-03-24 09:20:41 +0100

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -27,12 +27,29 @@ export default async function Home() {
         <Image className='dark:invert' src='/next.svg' alt='Next.js logo' width={180} height={38} priority />
         <ol className='list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
           <li className='mb-2 tracking-[-.01em]'>
-            Hi, welcome to the app {user?.user?.email}. Your are logged in with{' '}
-            <span className='font-bold'>{user?.user?.app_metadata?.provider}</span> Auth provider{' '}
+            {user?.user && (
+              <span>
+                Hi, welcome to the app {user?.user?.email}. Your are logged in with{' '}
+                <span className='font-bold'>{user?.user?.user_metadata?.provider}</span> Auth provider.
+              </span>
+            )}
+            {!user.user && (
+              <span>
+                Hi, welcome to supabase auth example.{' '}
+                <span className='font-bold'>Configure your supabase Auth and google cloud</span> to try signup, signin
+                to the app with email or google provider. (see{' '}
+                <a
+                  target='_blank'
+                  referrerPolicy='no-referrer'
+                  href='https://supabase.com/docs/guides/auth/social-login/auth-google?queryGroups=environment&environment=server'>
+                  docs
+                </a>
+                )
+              </span>
+            )}
             {/* <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold'>
               app/page.tsx
             </code> */}
-            .
           </li>
           <li className='tracking-[-.01em]'>Save and see your changes instantly.</li>
         </ol>
